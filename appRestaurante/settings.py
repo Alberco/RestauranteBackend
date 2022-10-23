5175dda3dc60715fe0c9fd9284ae62b700bfa325
Alberco <hyde2297@gmail.com>
2022-10-22 19:18:59 -0500

    asdasd

--- appRestaurante/settings.py
+++ appRestaurante/settings.py
@@ -85,9 +85,7 @@ WSGI_APPLICATION = 'appRestaurante.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url(
-        default='sqlite:///db.sqlite3',        
-        conn_max_age=600 )
+    'default': dj_database_url()
 }
 
 
